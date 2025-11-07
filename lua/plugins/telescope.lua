5a3d7fce1ab7d1e59a33fb2d3f3fac372fb2eb22
NandovdK <njvdkant@gmail.com>
2025-11-07 21:22:45 +0100

    feat: buffer keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,28 +12,28 @@ pcall(telescope.load_extension, "ui-select")
 
 local builtin = require("telescope.builtin")
 
-local set = vim.keymap.set
+local map = vim.keymap.set
 
-set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-set("n", "<leader>st", builtin.git_files, { desc = "[S]earch [F]iles" })
-set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+map("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+map("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+map("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>st", builtin.git_files, { desc = "[S]earch [F]iles" })
+map("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+map("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+map("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+map("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+map("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+map("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+map("n", "<leader>sb", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-set("n", "<leader>/", function()
+map("n", "<leader>/", function()
 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 		winblend = 10,
 		previewer = false,
 	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
 
-set("n", "<leader>s/", function()
+map("n", "<leader>s/", function()
 	builtin.live_grep({
 		grep_open_files = true,
 		prompt_title = "Live Grep in Open Files",
